af1c027d79ab14b127d4989a23dc5d9b86b4f552
github_username <you@domain.com>
2022-09-15 21:31:22 +0600

    second commit

--- variable.js
+++ variable.js
@@ -7,3 +7,27 @@ else{
     console.log
    ( 'My name is Alhaj');
 }
+
+var num = 220;
+if (num<=100){
+    console.log('your number')
+}
+else if(num>=120){
+    console.log('Up comming')
+}
+else{
+    console.log('Giving your data')
+}
+
+// math operator
+
+var Tamim =0;
+
+if (Tamim>0){
+    if (Tamim==20){
+    console.log('T20 captain')
+}
+}
+else{
+    console.log('This formate is not support')
+}
